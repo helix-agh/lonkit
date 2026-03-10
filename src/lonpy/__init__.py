7dcd9b0c2a6976cf7a71ab17bd859f055a44c72a
pFornagiel <pawel.fornagiel2@gmail.com>
2026-03-10 20:56:29 +0100

    fix: add BasinHoppingResult to __init__

--- src/lonpy/__init__.py
+++ src/lonpy/__init__.py
@@ -1,5 +1,10 @@
 from lonpy.lon import CMLON, LON, LONConfig
-from lonpy.sampling import BasinHoppingSampler, BasinHoppingSamplerConfig, compute_lon
+from lonpy.sampling import (
+    BasinHoppingResult,
+    BasinHoppingSampler,
+    BasinHoppingSamplerConfig,
+    compute_lon,
+)
 from lonpy.step_size import StepSizeEstimator, StepSizeEstimatorConfig, StepSizeResult
 from lonpy.visualization import LONVisualizer
 
@@ -7,6 +12,7 @@ __version__ = "0.1.0"
 __all__ = [
     "CMLON",
     "LON",
+    "BasinHoppingResult",
     "BasinHoppingSampler",
     "BasinHoppingSamplerConfig",
     "LONConfig",
