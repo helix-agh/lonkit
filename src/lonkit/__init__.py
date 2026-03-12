37917efdc1379127e8260b5f9e27478ee0de7852
WojtAcht <wachtelik@gmail.com>
2026-03-12 08:35:25 +0100

    fix: merge conflicts

--- src/lonkit/__init__.py
+++ src/lonkit/__init__.py
@@ -1,5 +1,10 @@
 from lonkit.lon import CMLON, LON, LONConfig
-from lonkit.sampling import BasinHoppingSampler, BasinHoppingSamplerConfig, compute_lon
+from lonkit.sampling import (
+    BasinHoppingResult,
+    BasinHoppingSampler,
+    BasinHoppingSamplerConfig,
+    compute_lon,
+)
 from lonkit.step_size import StepSizeEstimator, StepSizeEstimatorConfig, StepSizeResult
 from lonkit.visualization import LONVisualizer
 
@@ -7,6 +12,7 @@ __version__ = "0.1.0"
 __all__ = [
     "CMLON",
     "LON",
+    "BasinHoppingResult",
     "BasinHoppingSampler",
     "BasinHoppingSamplerConfig",
     "LONConfig",
