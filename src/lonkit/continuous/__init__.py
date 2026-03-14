09ed3dc71114f7f046a82c626f6809896c124625
WojtAcht <wachtelik@gmail.com>
2026-03-14 18:53:24 +0100

    feat: add subdirs for discrete and continuous problems

--- src/lonkit/continuous/__init__.py
+++ src/lonkit/continuous/__init__.py
@@ -4,19 +4,16 @@ from lonkit.continuous.sampling import (
     BasinHoppingSamplerConfig,
     compute_lon,
 )
-from lonkit.continuous.step_size import StepSizeEstimator, StepSizeEstimatorConfig, StepSizeResult
-from lonkit.lon import CMLON, LON, LONConfig
-from lonkit.visualization import LONVisualizer
+from lonkit.continuous.step_size import (
+    StepSizeEstimator,
+    StepSizeEstimatorConfig,
+    StepSizeResult,
+)
 
-__version__ = "0.1.0"
 __all__ = [
-    "CMLON",
-    "LON",
     "BasinHoppingResult",
     "BasinHoppingSampler",
     "BasinHoppingSamplerConfig",
-    "LONConfig",
-    "LONVisualizer",
     "StepSizeEstimator",
     "StepSizeEstimatorConfig",
     "StepSizeResult",
